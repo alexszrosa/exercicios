f9deaea9104a56910d8ed290562b625dc418436f
Alexandre Rosa <prcriandoseubot@gmail.com>
2025-08-14 14:33:47 -0300

    Resolvido os exercícios de 6 a 10

--- teste.py
+++ teste.py
@@ -3,47 +3,75 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-print("Exercício 1 - soma dois números")
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Inserir outro numero inteiro: "))
-resultado = numero_01 + numero_02
-print(resultado)
+# print("Exercício 1 - soma dois números")
+# numero_01 = int(input("Inserir um numero inteiro: "))
+# numero_02 = int(input("Inserir outro numero inteiro: "))
+# resultado = numero_01 + numero_02
+# print(resultado)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-print("Exercício 2 - resto da divisão por 5")
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = 5
-resultado = numero_01 % numero_02
-print(resultado)
+# print("Exercício 2 - resto da divisão por 5")
+# numero_01 = int(input("Inserir um numero inteiro: "))
+# numero_02 = 5
+r# esultado = numero_01 % numero_02
+# print(resultado)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-print("Exercício 3 - multiplique dois números")
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Inserir outro numero inteiro: "))
-resultado = numero_01 * numero_02
-print(resultado)
+# print("Exercício 3 - multiplique dois números")
+# numero_01 = int(input("Inserir um numero inteiro: "))
+# numero_02 = int(input("Inserir outro numero inteiro: "))
+# resultado = numero_01 * numero_02
+# print(resultado)
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-print("Exercício 4 - divisão inteira")
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Inserir outro numero inteiro: "))
-resultado = numero_01 // numero_02
-print(resultado)
+# print("Exercício 4 - divisão inteira")
+# numero_01 = int(input("Inserir um numero inteiro: "))
+# numero_02 = int(input("Inserir outro numero inteiro: "))
+# resultado = numero_01 // numero_02
+# print(resultado)
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-print("Exercício 5 - calcule o quadrado de um número")
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = 3
-resultado = numero_01 ** numero_02
-print(resultado)
+# print("Exercício 5 - calcule o quadrado de um número")
+# numero_01 = int(input("Inserir um numero inteiro: "))
+# numero_02 = 2
+# resultado = numero_01 ** numero_02
+# print(resultado)
 
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# print("Exercício 6 - calcule a soma de dois número float")
+# numero_01 = float(input("Inserir um numero flutantes: "))
+# numero_02 = float(input("Inserir um numero flutantes: "))
+# resultado = numero_01 + numero_02
+# print(resultado)
+
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# print("Exercício 7 - calcule a média dois número float")
+# numero_01 = float(input("Inserir um numero flutantes: "))
+# numero_02 = float(input("Inserir um numero flutantes: "))
+# resultado = (numero_01 + numero_02)/2
+# print(resultado)
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# print("Exercício 8 - calcule a potencia de um número float")
+# numero_01 = float(input("Inserir um numero flutantes: "))
+# numero_02 = float(input("Inserir a potencia: "))
+# resultado = numero_01 * numero_02
+# print(resultado)
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# print("Exercício 9 - temperatura de Celsius para Fahrenheit")
+# celsius = float(input("Digite a temperatura em Celsius: "))
+# fahrenheit = (celsius * 9/5) + 32
+# print(f"{celsius}°C é igual a {fahrenheit}°F")
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# print("Exercício 10 - calcule a área de um círculo")
+# raio_do_circulo = float(input("Digite o raio: "))
+# area_do_circulo = math.pi * raio_do_circulo ** 2
+# print(f"{area_do_circulo:.2f}")
+
 
 #raio_do_circulo = float(input("Digite o raio: "))
 #area_do_circulo = math.pi * raio_do_circulo ** 2
@@ -79,3 +107,4 @@ print(resultado)
 # 23: Calculadora Simples
 # 24: Classificador de Números
 # 25: Conversão de Tipo com Validação
+
